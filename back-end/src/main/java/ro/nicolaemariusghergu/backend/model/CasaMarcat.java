7dc234eade4b830adc447d19e243c53f090dfae2
nicugnm <nicolae.ghergu@s.unibuc.ro>
2021-10-08 22:37:31 +0300

    Project is working now.

--- back-end/src/main/java/ro/nicolaemariusghergu/backend/model/CasaMarcat.java
+++ back-end/src/main/java/ro/nicolaemariusghergu/backend/model/CasaMarcat.java
@@ -5,14 +5,12 @@ import lombok.Setter;
 import ro.nicolaemariusghergu.backend.model.enums.TypePay;
 import ro.nicolaemariusghergu.backend.model.enums.TypeStatus;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @javax.persistence.Entity
+@Table(name = "case_marcat")
 public class CasaMarcat extends Entity {
 
     @Id
@@ -25,11 +23,11 @@ public class CasaMarcat extends Entity {
     @Column(name = "bani_locali_casa_marcat")
     private Double localMoney;
 
-    @Column(name = "type_pay_id", nullable = false)
+    @JoinColumn(name = "type_pay_id", nullable = false)
     @ManyToOne
     private TypePay typePay;
 
-    @Column(name = "type_status_id", nullable = false)
+    @JoinColumn(name = "type_status_id", nullable = false)
     @ManyToOne
     private TypeStatus typeStatus;
 }
--- back-end/src/main/resources/db/V1_0_0_CREATE_DATABASE_CASA_MARCAT.sql
+++ back-end/src/main/resources/db/V1_0_0_CREATE_DATABASE_CASA_MARCAT.sql
@@ -1,6 +1,42 @@
 CREATE TABLE IF NOT EXISTS
+    type_pay(
+                id SERIAL, type VARCHAR,
+                PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS
+    type_status(
+                   id SERIAL, type VARCHAR,
+                   PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS
     case_marcat(
-        id VARCHAR, numar_casa_marcat SERIAL,
-        bani_locali_casa_marcat FLOAT, type_id VARCHAR,
-        type_status_id VARCHAR
-    );
+        id SERIAL, numar_casa_marcat SERIAL,
+        bani_locali_casa_marcat FLOAT, type_pay_id SERIAL,
+        type_status_id SERIAL,
+        PRIMARY KEY (id),
+        CONSTRAINT fk_type_pay FOREIGN KEY (type_pay_id) REFERENCES type_pay(id),
+        CONSTRAINT fk_type_status FOREIGN KEY (type_status_id) REFERENCES type_status(id)
+);
+
+INSERT INTO type_pay(type)
+VALUES ('CASH');
+INSERT INTO type_pay(type)
+VALUES ('CARD');
+INSERT INTO type_pay(type)
+VALUES ('AMPRENTA');
+
+INSERT INTO type_status(type)
+VALUES ('BLOCATA');
+INSERT INTO type_status(type)
+VALUES ('FUNCTIONALA');
+INSERT INTO type_status(type)
+VALUES ('ASTEAPTA_OPERATOR');
+
+INSERT INTO case_marcat(numar_casa_marcat, bani_locali_casa_marcat)
+VALUES (1, 100);
+INSERT INTO case_marcat(numar_casa_marcat, bani_locali_casa_marcat)
+VALUES (2, 200);
+INSERT INTO case_marcat(numar_casa_marcat, bani_locali_casa_marcat)
+VALUES (3, 300);

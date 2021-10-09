e90d14fa0c8d1207947c15aad56f87ce6759d716
nicugnm <nicolae.ghergu@s.unibuc.ro>
2021-10-09 12:13:33 +0300

    Exception handler and debugging message when throws.

--- back-end/src/main/java/ro/nicolaemariusghergu/backend/exceptions/ResourceNotFoundException.java
+++ back-end/src/main/java/ro/nicolaemariusghergu/backend/exceptions/ResourceNotFoundException.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-
 @NoArgsConstructor
 public class ResourceNotFoundException extends RuntimeException {
 

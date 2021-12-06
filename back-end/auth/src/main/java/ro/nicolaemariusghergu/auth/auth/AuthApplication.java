d44fc9837d0503a292489a583da1e9b1042a45a5
nycuro <nicolae.ghergu@s.unibuc.ro>
2021-12-06 15:26:26 +0200

    Let's use back-end with microservices. Also, AUTH SOON!

---
+++ back-end/auth/src/main/java/ro/nicolaemariusghergu/auth/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package ro.nicolaemariusghergu.auth.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}

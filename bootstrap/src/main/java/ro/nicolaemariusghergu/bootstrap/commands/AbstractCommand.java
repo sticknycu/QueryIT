36e41db4c135951fe2c097144f1f74231feb47f6
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-04-09 23:38:23 +0300

    Add queryit-data and queryit-auth-frontend

--- bootstrap/src/main/java/ro/nicolaemariusghergu/bootstrap/commands/AbstractCommand.java
+++
@@ -1,7 +0,0 @@
-package ro.nicolaemariusghergu.bootstrap.commands;
-
-public abstract sealed class AbstractCommand
-        permits StartupBackendCoreCommand {
-
-    public abstract String executeCommand(ICommand.CommandType commandType);
-}
